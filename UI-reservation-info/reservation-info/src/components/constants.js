6e25666015e6c229538c9930ae0f6f06d2197040
Ravindra <rramcha@users.noreply.github.com>
2020-03-26 23:07:19 -0500

    Create constants.js

---
+++ UI-reservation-info/reservation-info/src/components/constants.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+    BASE_URI: '/template',
+    RESERVATION_TITLE: 'User Reservation Details',
+    NO_GUESTS_FOUND: 'No Guests at this moment.',
+    PRICE_BREAKDOWN_TITLE: 'Price Breakdown Summary',
+    PRICE_BREAKDOWN_FOOTER: '*All prices are in USD',
+    GUEST_TYPE: {
+        PRIMARY: 'Primary',
+        ADDITIONAL: 'Additional'
+    },
+    URI: {
+        GUEST_DETAILS: 'guest-details.html',
+        PRICE_BREAK_DOWN: 'price-break-down.html',
+        RESERVATIONS_HOME: 'reservations.html'
+    },
+    BTN_LABELS: {
+        PREVIOUS: 'Previous',
+        SEARCH: 'Search',
+        REFRESH: 'Refresh'
+    }
+
+}
